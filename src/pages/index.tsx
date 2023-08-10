35656bb87ef3130b29567f35a3382645d1603180
Datta-Landage <dattalandge001@gmail.com>
2023-08-10 15:46:17 +0530

    frontend socket

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,101 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+const socket = io("http://3.110.120.134:3000/");
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function MyComponent() {
+  const [data, setData] = useState({});
 
-export default function Home() {
+  const handleSend = () => {
+    socket.emit("businessId", {
+      tableId: "0000",
+      businessId: "9999",
+      userId: "5455 ",
+      hiCode: "3333",
+      cart: {
+        "63c79af83cb5dc11d8f53574": {
+          dishData: {
+            delayTime: null,
+            businessId: "63c0f3e4ee03bd948ba6522a",
+            name: "Mutton Soup",
+            price: 350,
+            dishType: "Non veg",
+            categories: [
+              {
+                id: "63da1d24311e85f001f8bb58",
+                name: "Soups & Salads",
+              },
+              {
+                id: "63db8198311e85f001f8c0eb",
+                name: "Our Favorites ",
+              },
+              {
+                id: "641d9a9d311e85f001f93fe2",
+                name: "Specials ",
+              },
+            ],
+            portions: [],
+            extras: [
+              {
+                numOptions: 0,
+                numSelections: 0,
+                compulsory: 0,
+                items: [],
+              },
+            ],
+            filters: {
+              timeToConsume: "",
+              portionBestSuited: "",
+              levelOfSpice: "",
+              bestOccasion: "",
+              texture: "",
+              time: "",
+              bestWeather: "",
+              dishes: [],
+            },
+            images: [
+              "https://storage.hipal.life/minio/assets/4200f06b-bf17-4764-83d8-00f7d07f4f56.jpeg",
+              "https://storage.hipal.life/minio/assets/b3f761a3-794c-4078-a0a0-70d016e15ee0.jpeg",
+              "https://storage.hipal.life/minio/assets/be5ebf07-7745-4a86-9645-4834d7e7b283.jpeg",
+              "https://storage.hipal.life/minio/assets/a2d55b44-001c-42d1-b532-e32de5b7b954.jpeg",
+              "https://storage.hipal.life/minio/assets/b6bd0543-0672-46b9-b03c-c3629dedbd43.jpeg",
+            ],
+            description:
+              "Slow cooked mutton bone marrow soup served with bread sticks.",
+            tags: [],
+            links: [],
+            version: 5,
+            visibility: true,
+            availability: true,
+            id: "63c79af83cb5dc11d8f53574",
+          },
+          dishId: "63c79af83cb5dc11d8f53574",
+          variants: [],
+          userDetails: {
+            name: "suhas",
+            email: "string",
+            phone: "string",
+            role: "string",
+            id: "string",
+            userDiscription: "string",
+          },
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    // listen for the 'message' event
+    socket.on("businessId", (data: any) => {
+      console.log(data); // log the received data
+      setData(data); // set the received data in state
+    });
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <button onClick={handleSend}>Send Object</button>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+    </div>
+  );
 }
